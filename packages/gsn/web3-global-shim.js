324e583e5f80a37e8979430734fc617703ca9db5
jorisstrakeljahn <joris.strakeljahn@web.de>
2025-10-21 14:49:59 +0200

    chore: remove outdated comments from web3-global-shim.js

--- packages/gsn/web3-global-shim.js
+++ packages/gsn/web3-global-shim.js
@@ -1,6 +1,4 @@
 // web3-global-shim.js
-// Macht den Modul-Import "web3" zu einem Proxy auf das globale Web3,
-// das du per <script src="/gsn/web3.min.js"> lädst.
 const W = (typeof window !== "undefined" && window.Web3) || undefined;
 if (!W) {
     throw new Error('Global Web3 not found. Load "/gsn/web3.min.js" BEFORE gsn-umd.js');
